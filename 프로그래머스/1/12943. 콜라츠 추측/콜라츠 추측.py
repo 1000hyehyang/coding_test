376cddd546c3b6f5f3cc8909163f4e37914d1c46
1000hyehyang <ducogus12@gmail.com>
2026-08-29 22:22:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,24 @@
+def solution(num):
+    answer = 0
+    cnt = 0
+    
+    if num == 1:
+        return 0
+    
+    while num != 1:
+        if cnt == 500:
+            return -1
+        
+        if num % 2 == 0:
+            num //= 2
+            cnt += 1
+        else:
+            num = num * 3 + 1
+            cnt += 1
+        
+        if num == 1:
+            break
+    
+    answer = cnt 
+    
+    return answer
